f8f1e06561bb76a1d0f44ed8d9f774194214ad08
wulianwei <wulianwei@thit.com.cn>
2019-05-21 10:44:20 +0800

    FruitAccess

---
+++ src/main/java/wlw/prac/SpringbootApplication.java
@@ -0,0 +1,14 @@
+package wlw.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+public class SpringbootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
